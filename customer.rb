06622927d241160705a9947c7aadf24c040ea4b2
Yuanchieh <sj82516@gmail.com>
2021-05-02 16:34:03 +0800

    refactor: move method to rental

--- customer.rb
+++ customer.rb
@@ -17,6 +17,21 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
+
+  def charge
+    result = 0
+    case movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
 end
 
 class Customer
@@ -37,7 +52,7 @@ class Customer
     @rentals.each do |element|
       this_amount = 0
 
-      this_amount = amount_for(element)
+      this_amount = element.charge
 
       frequent_renter_points += 1
       if element.movie.price_code == Movie::NEW_RELEASE &&
@@ -55,20 +70,5 @@ class Customer
   end
 
   private
-
-  def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-    result
-  end
 
 end
