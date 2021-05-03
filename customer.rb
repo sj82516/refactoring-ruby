4e99642cc80c190f48f5f29c9bf7ccf05c208c66
Yuanchieh <sj82516@gmail.com>
2021-05-04 05:48:30 +0800

    refactor: add state pattern to extract charge logic

--- customer.rb
+++ customer.rb
@@ -3,30 +3,51 @@ class Movie
   NEW_RELEASE = 1
   CHILDRENS = 2
 
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
   def initialize(title, price_code)
-    @title, @price_code = title, price_code
+    @title, self.price_code = title, price_code
+  end
+
+  def price_code=(value)
+    @price_code = value
+    @price = case price_code
+             when REGULAR then
+               RegularPrice.new
+             when NEW_RELEASE then
+               NewReleasePrice.new
+             when CHILDRENS then
+               ChildrenPrice.new
+             end
   end
 
   def charge(days_rented)
-    result = 0
-    case price_code
-    when REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when NEW_RELEASE
-      result += days_rented * 3
-    when CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
     (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
+end
+
+class RegularPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    days_rented * 3
+  end
+end
+
+class ChildrenPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
   end
 end
 
