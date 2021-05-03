3249902cc08268667286ba7ff0c2f43a7dec9eac
Yuanchieh <sj82516@gmail.com>
2021-05-04 05:27:12 +0800

    refactor: move charge logic to Movie

--- customer.rb
+++ customer.rb
@@ -9,6 +9,21 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code
   end
+
+  def charge(days_rented)
+    result = 0
+    case price_code
+    when REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when NEW_RELEASE
+      result += days_rented * 3
+    when CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
 end
 
 class Rental
@@ -19,18 +34,7 @@ class Rental
   end
 
   def charge
-    result = 0
-    case movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @movie.charge(@days_rented)
   end
 
   def frequent_renter_points
